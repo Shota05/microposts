57717103cb4549bae1c9caa3c7cd162680aaf5cb
shota05 <komatsu.shota@gmail.com>
2015-10-05 11:19:37 +0000

    add sign sign up and user detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'signup',  to: 'users#new'
+  resources :users
   get 'static_pages/home'
   root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
