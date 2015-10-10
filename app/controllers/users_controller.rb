fe12e021c75928cd460746d239d0e0d8efb590b0
shota05 <komatsu.shota@gmail.com>
2015-10-10 09:50:13 +0000

    kadai1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,25 @@ class UsersController < ApplicationController
    @user = User.find(params[:id])
   end
   
+  def edit 
+  end
+  
+  def update
+    if current_user.update(user_edit_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      flash[:success] = "更新しました!"
+      redirect_to user_path 
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
+      session[:user_id] = @user.id
       redirect_to @user # ここを修正
     else
       render 'new'
@@ -22,5 +37,8 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+  def user_edit_params
+    params.require(:user).permit(:name, :email, :address, :profile)
   end
 end
